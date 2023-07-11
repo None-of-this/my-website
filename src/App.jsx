9a4027a423f7c674edfb9350a44516120a0467dc
None Rival <none.rival.pro@gmail.com>
2023-07-11 16:48:35 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import profilePicture from '/profilePicture.jpg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={profilePicture} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>None Rival</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
